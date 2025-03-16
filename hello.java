7360b04229fd7845ebd72bd3214104a8096ab726
snehareddyz <153310609+snehareddyz@users.noreply.github.com>
2025-03-16 21:49:09 +0530

    day 1
    here is the day 1 of my java this repository contains Java programs from my first day of learning Java. It includes fundamental concepts like primitive and non-primitive data types and basic Java program.

---
+++ hello.java
@@ -0,0 +1,37 @@
+public class hello {
+	public static void main (String[] args){
+	int age = 21;
+	int year = 2025;//---integer---
+	int quantity = 1;
+	
+	double price = 19.99;
+	double gpa = 3.4;//---double---
+	double temperature = -12.5;
+
+	char grade = 'A';
+	char symbol = '!';//---char----
+	char currency = '$';
+
+	boolean isStudent = false;
+	boolean forSale = false;//---boolean----(all this 4 are primitive data types)
+	boolean isOnline = true;
+
+	//if(isStudent){
+	//	System.out.println("you are a student!");
+	//}//-----in if els kinda statements we use booleans---
+	//else{
+	//	System.out.println("you are not a student!");
+	String name = "sneha";
+	String food = "chicken";
+	String email = "reddyzsnha@gmail.com";///---string--(reference data type)-----
+	String car = "Porsche";
+	String color = "silver";
+	String model = "panamara";
+
+	//System.out.print("hello" + name + " ");
+	//System.out.print("your score is " + gpa + " ," );
+	//System.out.print("you are " + age + " years old" + " ,");
+	//System.out.print("you like " + food + " ," );
+	System.out.print("your choice is a " + color + " " + model + " " + car);}//-- let us choose //somthin
+
+}
